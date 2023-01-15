450061a05a179183c726859918340dbcc3fd67cd
KonradRudnicki <97458824+KonradRudnicki@users.noreply.github.com>
2023-01-15 17:10:14 +0100

    Initial commit

---
+++ src/main/java/com/pwrjavamasters/cardwargame/CardWarGameApplication.java
@@ -0,0 +1,13 @@
+package com.pwrjavamasters.cardwargame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardWarGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardWarGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pwrjavamasters/cardwargame/CardWarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwrjavamasters.cardwargame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardWarGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
