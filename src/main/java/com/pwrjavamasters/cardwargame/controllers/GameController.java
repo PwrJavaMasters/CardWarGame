0626624413026335f5ae479b2088f9a84924aaa8
konradrudnicki <konradrudnicki0@gmail.com>
2023-01-17 16:04:05 +0100

    Creating controllers

--- src/main/java/com/pwrjavamasters/cardwargame/controllers/GameController.java
+++ src/main/java/com/pwrjavamasters/cardwargame/controllers/GameController.java
@@ -1,4 +1,16 @@
 package com.pwrjavamasters.cardwargame.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GameController {
+
+    @RequestMapping("/game")
+    public String getGamePage(Model model){
+
+        return "game";
+    }
 }
